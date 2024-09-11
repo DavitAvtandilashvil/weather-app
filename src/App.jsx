504a93f56204c802d900816580adc18c901de1b7
merabkandelaki <100033268+merabkandelaki@users.noreply.github.com>
2024-09-11 18:44:11 +0400

    resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout'; 
+import Home from './pages/Home'; 
+ 
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
