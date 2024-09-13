26c061ad1d8af97fe8127724c6d1c7a606933ad0
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-13 23:44:21 +0400

    create single day temperature block

---
+++ src/components/SingleDayWeatherComponent.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import sunIcon from "../assets/icons/filled-sun.svg";
+import dayTempIcon from "../assets/icons/dayTempIcon.svg";
+import nightTempIcon from "../assets/icons/nightTempIcon.svg";
+
+const SingleDayWeatherComponent = ({
+  date,
+  dayName,
+  tempIcon,
+  dayTemp,
+  nightTemp,
+}) => {
+  return (
+    <div className="w-[152px] h-[184px] rounded-[12px] px-[8px] pt-[20px] pb-[14px] flex flex-col gap-[10px] bg-[#B3B3B366] text-white items-center">
+      {/* day name and date */}
+      <div className="flex flex-col gap-[5px]">
+        <p className="leading-[19px] text-center">14 სექტემბერი</p>
+        <p className="text-[12px] leading-[14px] text-lightGrey text-center">
+          ორშაბათი
+        </p>
+      </div>
+
+      {/* weather icon */}
+      <img src={sunIcon} className="w-[40px]" />
+
+      {/* temperatures */}
+      <div className="flex gap-[12px] items-center">
+        {/* day temp */}
+        <div className="flex flex-col items-center">
+          <p className="text-[28px] leading-[33px]">31&deg;</p>
+          <img src={dayTempIcon} />
+        </div>
+
+        {/* divider */}
+        <div className="h-[24px] w-[1px] bg-[#BEBEBE] -mt-[10px]" />
+
+        {/* night temp */}
+        <div className="flex flex-col items-center">
+          <p className="text-[28px] leading-[33px]">31&deg;</p>
+          <img src={nightTempIcon} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleDayWeatherComponent;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        lightGrey: "#D7D7D7"
+      }
+    },
   },
   plugins: [],
 };
