70aa23c9c2c8717b5606b39f60ce53c0c28149d4
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-14 23:38:19 +0400

    create service and custom hook for daily weather

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+
+//ტაბის შეცვალზე და ისევ ამ ტაბზე დაბრუნებისას რომ არ დაიწყოს ლოუდინგი
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DailyWeather from '../components/DailyWeather';
 import HourlyWeather from '../components/HourlyWeather';
 import ClimateIndicators from '../components/ClimateIndicators';
 import WeekWeatherWidget from '../components/WeekWeatherWidget';
+import useDailyWeather from '../hooks/useDailyWeather';
 
 const Home = () => {
+
+  const { data, isError, error, isLoading } = useDailyWeather()
+
+ if(isLoading){
+  return <div>retunadsadadasdasda </div>
+ }
+
+ if(isError){
+  return <div className='text-[#FF0000] text-[30px]'>{error.message}</div>
+ }
+
+ useEffect(()=>{
+  console.log(data);
+
+ },[data])
+
+
   return (
     <div className="px-[16px] bg-cover bg-no-repeat min-h-screen flex gap-[80px]">
       <div>
